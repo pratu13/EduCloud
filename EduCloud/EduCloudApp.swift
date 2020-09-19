8a7e6c91bcf72892333db949d7e1770b80c3e377
Pratyush Duklan <pratu0915@gmail.com>
2020-09-19 12:45:54 +0530

    Initial Commit

---
+++ EduCloud/EduCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  EduCloudApp.swift
+//  EduCloud
+//
+//  Created by Pratyush Sharma on 19/09/20.
+//
+
+import SwiftUI
+
+@main
+struct EduCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
